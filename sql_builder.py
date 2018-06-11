b1a504ec905443ac2a809d6b8caddc9cac294b8e
mardong <mardong_@inbox.ru>
2018-06-12 05:19:55 +0700

    Реализованы опции 'выходной файл', 'подключение к БД'

--- sql_builder.py
+++ sql_builder.py
@@ -21,27 +21,55 @@ def datatype_to_sqltype(field_name, field_value, dbtype="postgres"):
 def dict_to_fields(inp_dict):
 	return map(lambda k: fix_json_field_name(k)+" "+datatype_to_sqltype(k, inp_dict[k]), inp_dict.keys() )
 
-#TODO: параметры для верификации базы, для подключения к БД и выполнения DDL, alter table
-#parser = argparse.ArgumentParser(description='postall SQL database init')
+def sql_table(tablename, fields):
+	return "create table if not exists %s(\n" % (tablename,) + ", \n".join( fields ) + ");\n"
 
-out = open("db.sql","w")
-out.write( "create table if not exists LETTER_INFO(\n"  +
-	", \n".join( dict_to_fields(defconf.LETTER_DB_FIELDS)+dict_to_fields(defconf.LETTER_DEFAULT)  ) +
-	");\n" )
-out.write("\n")
-out.write( "create table if not exists REESTR_INFO(\n"  +
-	", \n".join( dict_to_fields(defconf.REESTR_DB_FIELDS)  ) +
-	");\n" )
-out.write("\n")
+def dump_sql(sql, out, db):
+	if out:
+		out.write(sql)
+	if db:
+		cur = db.cursor()
+		cur.execute(sql)
+		db.commit()
+
+#TODO: РїР°СЂР°РјРµС‚СЂС‹ РґР»СЏ РІРµСЂРёС„РёРєР°С†РёРё Р±Р°Р·С‹, alter table, РІС‹Р±РѕСЂ С‚РёРїР° СЃСѓР±Рґ
+parser = argparse.ArgumentParser(description='postall SQL database initializer')
+parser.add_argument("-o","--out", type=unicode, default="-", help="output file for sql code" )
+parser.add_argument("-db","--database", type=unicode, default=argparse.SUPPRESS, help="database connection string")
+parser.add_argument("-a","--alter", action="store_true", help="alter columns of tables")
+parser.add_argument("-v","--verify", action="store_true", help="verify tables")
+args = vars(parser.parse_args())
+
+out = None
+db = None
+if "out" in args:
+	if args["out"]=="-":
+		import sys
+		out = sys.stdout
+	else:
+		out = open(args["out"],"w")
+if "database" in args:
+	import psycopg2
+	db = psycopg2.connect(args["database"])
+
+###
+sql = sql_table("LETTER_INFO", dict_to_fields(defconf.LETTER_DB_FIELDS)+dict_to_fields(defconf.LETTER_DEFAULT) )
+dump_sql(sql, out, db)
+###
+sql = sql_table("REESTR_INFO", dict_to_fields(defconf.REESTR_DB_FIELDS) )
+dump_sql(sql, out, db)
+###
 contragent_struct = copy.copy(defconf.CONTRAGENT_DB_FIELDS)
 for k,v in defconf.LETTER_DEFAULT.iteritems():
 	if k.endswith("-to"):
 		contragent_struct[k] = v
-out.write( "create table if not exists CONTRAGENT_DICT(\n"  +
-	", \n".join( dict_to_fields(contragent_struct)  ) +
-	");\n" )
-out.write("\n")
-out.write( "create table if not exists USER_DICT(\n"  +
-	", \n".join( dict_to_fields(defconf.USER_DB_FIELDS)  ) +
-	");\n" )
-out.close()
+sql = sql_table("CONTRAGENT_DICT", dict_to_fields(contragent_struct) )
+dump_sql(sql, out, db)
+###
+sql = sql_table("USER_DICT", dict_to_fields(defconf.USER_DB_FIELDS) )
+dump_sql(sql, out, db)
+###
+if out:
+	out.close()
+if db:
+	db.close()
