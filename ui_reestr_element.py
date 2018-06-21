2d72e68b301ed1b2036b6b93bf423ecf8d12342b
mardong <mardong_@inbox.ru>
2018-06-21 12:22:17 +0700

    Исправлено определение GUI_DEF Добавлен параметр user_ident (UserIdentifier) для работы административных функций Добавлена кнопка "Редактировать"

--- ui_reestr_element.py
+++ ui_reestr_element.py
@@ -16,20 +16,24 @@ COLORS = (
 class UIReestrElement(tk.Frame):
 	"""UI класс представления реестра
 	"""
-	GUI_DEF = [
-		{'key': 'db_reestr_id', 'title': u'Номер', 'type': 'text', 'edit': False, 'maxsize': 10, 'row': 0},
-		{'key': 'db_create_date', 'title': u'Дата создания', 'type': 'date', 'edit': False, 'maxsize': 12, 'row': 0},
-		{'key': 'batch-name', 'title': u'Номер(сайт)', 'type': 'text', 'edit': False, 'maxsize': 10, 'row': 1},
-		{'key': 'list-number-date', 'title': u'Дата отправки', 'type': 'date', 'edit': True, 'maxsize': 12, 'row': 1},
-		{'key': 'db_letter_count', 'title': u'Кол-во', 'type': 'integer', 'edit': False, 'maxsize': 10, 'row': 0},
-		{'key': 'with-simple-notice', 'title': u'С уведомл.', 'type': 'bool', 'edit': False, 'maxsize': 10, 'row': 1},
-		{'key': 'db_comment', 'title': u'Комм.', 'type': 'text', 'edit': False, 'maxsize': 20, 'row': 0},
-		{'key': 'fio', 'title': u'ФИО', 'type': 'text', 'edit': False, 'maxsize': 20, 'row': 1},
-	]
 
-	def __init__(self, master, bgcolor=0, action_callback=None):
+	def __init__(self, master, user_ident, bgcolor=0, action_callback=None):
+		self.GUI_DEF = [
+			{'key': 'db_reestr_id', 'title': u'Номер', 'type': 'text', 'edit': False, 'maxsize': 10, 'row': 0},
+			{'key': 'db_create_date', 'title': u'Дата создания', 'type': 'date', 'edit': False, 'maxsize': 12,
+			 'row': 0},
+			{'key': 'batch-name', 'title': u'Номер(сайт)', 'type': 'text', 'edit': False, 'maxsize': 10, 'row': 1},
+			{'key': 'list-number-date', 'title': u'Дата отправки', 'type': 'date', 'edit': True, 'maxsize': 12,
+			 'row': 1},
+			{'key': 'db_letter_count', 'title': u'Кол-во', 'type': 'integer', 'edit': False, 'maxsize': 10, 'row': 0},
+			{'key': 'with-simple-notice', 'title': u'С уведомл.', 'type': 'bool', 'edit': False, 'maxsize': 10,
+			 'row': 1},
+			{'key': 'db_comment', 'title': u'Комм.', 'type': 'text', 'edit': False, 'maxsize': 20, 'row': 0},
+			{'key': 'fio', 'title': u'ФИО', 'type': 'text', 'edit': False, 'maxsize': 20, 'row': 1},
+		]
 		self.reestr_info = {}
 		self.action_callback = action_callback
+		self.user_ident = user_ident
 		#create widgets
 		tk.Frame.__init__(self, master)
 		self["bg"] = COLORS[bgcolor][0]
@@ -56,14 +60,18 @@ class UIReestrElement(tk.Frame):
 			c['widget'] = ent
 			gridcol[c['row']] += 2
 		#кнопки
-		self.btn_date = tk.Button(self, text = u"Изменить дату отправки", command=self.onClickDate, bg=COLORS[bgcolor][0])
-		self.btn_date.grid({"column":max(gridcol.values())+1, "row":0, "sticky":"NSEW"})
-		#
-		self.btn_lock = tk.Button(self, text=u"Заблокировать окончательно", command=self.onClickLock, bg=COLORS[bgcolor][0])
-		self.btn_lock.grid({"column":max(gridcol.values())+2, "row":1, "sticky":"NSEW"})
-		#
+		#редактирование
+		self.btn_edit = tk.Button(self, text=u"Редактировать", command = self.onClickEdit, bg=COLORS[bgcolor][0])
+		self.btn_edit.grid({"column":max(gridcol.values())+1, "row":0, "sticky":"NSEW"})
+		#удаление
 		self.btn_delete = tk.Button(self, text=u"Удалить", command = self.onClickDelete, bg=COLORS[bgcolor][0])
 		self.btn_delete.grid({"column":max(gridcol.values())+2, "row":0, "sticky":"NSEW"})
+		#дата отправки
+		self.btn_date = tk.Button(self, text = u"Изменить дату отправки", command=self.onClickDate, bg=COLORS[bgcolor][0])
+		self.btn_date.grid({"column":max(gridcol.values())+1, "row":1, "sticky":"NSEW"})
+		#заблокиировать
+		self.btn_lock = tk.Button(self, text=u"Заблокировать окончательно", command=self.onClickLock, bg=COLORS[bgcolor][0])
+		self.btn_lock.grid({"column":max(gridcol.values())+2, "row":1, "sticky":"NSEW"})
 	def set_data(self, reestr_info):
 		""" Скопировать данные реестра, заполнить entry
 		"""
@@ -86,18 +94,23 @@ class UIReestrElement(tk.Frame):
 	def set_lock(self, lock_state):
 		""" Установить доступность элементов согласно статусу блокировки
 		"""
+		self.btn_edit["state"] = "disabled"
 		self.btn_date["state"] = "disabled"
 		self.btn_lock["state"] = "disabled"
 		self.btn_delete["state"] = "disabled"
 		for c in self.GUI_DEF:
 			c["widget"].config(state='readonly')
 		if lock_state == LOCK_STATE_FREE:
+			self.btn_edit["state"] = "normal"
 			self.btn_delete["state"] = "normal"
 			for c in self.GUI_DEF:
 				if c["edit"]:
 					c["widget"].config(state='normal')
 		if lock_state == LOCK_STATE_BATCH:
+			self.btn_edit["state"] = "normal"
 			self.btn_date["state"] = "normal"
+			if self.user_ident.is_admin() == 1:
+				self.btn_lock["state"] = "normal"
 		if lock_state == LOCK_STATE_FINAL:
 			pass
 	def sync(self):
@@ -121,3 +134,6 @@ class UIReestrElement(tk.Frame):
 	def onClickDelete(self):
 		if self.action_callback:
 			self.action_callback("DELETE", self.reestr_info)
+	def onClickEdit(self):
+		if self.action_callback:
+			self.action_callback("EDIT", self.reestr_info)
