7e2ed294afac7bf58623a2134bb0625f49001c75
mardong <mardong_@inbox.ru>
2018-06-22 11:41:13 +0700

    Убрана привязка к dbstorage

--- user_identifier.py
+++ user_identifier.py
@@ -3,11 +3,12 @@
 class UserIdentifier:
 	""" Класс информации о пользователе
 	"""
-	def __init__(self, dbstorage, user_id):
-		self.user_info = {}
-		inf = dbstorage.get_user_info(user_id)
-		if inf[1] == "":
-			self.user_info = inf[0]
+	def __init__(self, user_info):
+		""" Конструктор
+		
+		:param user_info: dict() - configuration.USER_DB_FIELDS
+		"""
+		self.user_info = user_info
 	def get_fio(self):
 		if "fio" in self.user_info:
 			return self.user_info["fio"]
