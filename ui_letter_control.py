6959979ac875e77b59abb1c05eab07ad54acd5b4
mardong <mardong_@inbox.ru>
2018-06-26 14:42:20 +0700

    Добавлен класс управления для UIEditWindow
    добавлены операции:
    - обновить список
    - присвоить всем номера
    - удалить номера у всех

---
+++ ui_letter_control.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import Tkinter as tk
+
+class UILetterControl(tk.Frame):
+	"""UI класс для разных операций в UIEditWindow
+	"""
+	def __init__(self, master, action_callback=None):
+		"""
+		
+		:param master:
+		:param action_callback:
+		"""
+		tk.Frame.__init__(self, master)
+		self.action_callback = action_callback
+		#кнопки
+		self.btn_refresh = tk.Button(self, text=u"Обновить", command=self.onClickRefresh)
+		self.btn_refresh.grid({"column": 1, "row": 0, "sticky": "NSEW"})
+		self.btn_add_barcodes = tk.Button(self, text = u"Присвоить номера всем", command=self.onClickAdd)
+		self.btn_add_barcodes.grid({"column": 2, "row":0, "sticky":"NSEW"})
+		self.btn_del_barcodes = tk.Button(self, text=u"Удалить номера у всех", command=self.onClickDel)
+		self.btn_del_barcodes.grid({"column": 3, "row": 0, "sticky": "NSEW"})
+	def onClickAdd(self):
+		if self.action_callback:
+			self.action_callback("BARCODE_ADD_ALL", None)
+	def onClickDel(self):
+		if self.action_callback:
+			self.action_callback("BARCODE_DEL_ALL", None)
+	def onClickRefresh(self):
+		if self.action_callback:
+			self.action_callback("REFRESH", None)
