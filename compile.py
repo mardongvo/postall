90af1538c8f69435780a04eee84e96054a3302d8
mardong <mardong_@inbox.ru>
2018-06-27 14:01:16 +0700

    Добавлен скрипт компиляции в exe

---
+++ compile.py
@@ -0,0 +1,27 @@
+import sys
+from distutils.core import setup
+
+sys.path.insert(0, "C:\\Python27\\Lib\\site-packages\\reportlab")
+setup(
+	console=[
+	{"script":"mainapp.py",
+	"icon_resources": [(0, "icon.ico")]
+	}
+	],
+	options={
+			"py2exe":{
+					"packages": [
+    'reportlab',
+    'reportlab.graphics.charts',
+    'reportlab.graphics.samples',
+    'reportlab.graphics.widgets',
+    'reportlab.graphics.barcode',
+    'reportlab.graphics',
+    'reportlab.lib',
+    'reportlab.pdfbase',
+    'reportlab.pdfgen',
+    'reportlab.platypus',
+		]
+			}
+	}	
+)
