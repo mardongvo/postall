0eb44b9bbdb116a9dd97cffc7b5e79df7e3abb27
mardong <mardong_@inbox.ru>
2018-06-30 13:17:20 +0700

    Исправлена скрипт компиляции

--- compile.py
+++ compile.py
@@ -1,27 +1,25 @@
 import sys
 from distutils.core import setup
+import py2exe
 
 sys.path.insert(0, "C:\\Python27\\Lib\\site-packages\\reportlab")
 setup(
-	console=[
-	{"script":"mainapp.py",
-	"icon_resources": [(0, "icon.ico")]
-	}
+	console=[{"script":"mainapp.py","icon_resources": [(0,"icon.ico")]}
 	],
 	options={
-			"py2exe":{
-					"packages": [
-    'reportlab',
-    'reportlab.graphics.charts',
-    'reportlab.graphics.samples',
-    'reportlab.graphics.widgets',
-    'reportlab.graphics.barcode',
-    'reportlab.graphics',
-    'reportlab.lib',
-    'reportlab.pdfbase',
-    'reportlab.pdfgen',
-    'reportlab.platypus',
-		]
-			}
+		"py2exe":{
+			"packages": [
+				'reportlab',
+				'reportlab.graphics.charts',
+				'reportlab.graphics.samples',
+				'reportlab.graphics.widgets',
+				'reportlab.graphics.barcode',
+				'reportlab.graphics',
+				'reportlab.lib',
+				'reportlab.pdfbase',
+				'reportlab.pdfgen',
+				'reportlab.platypus',
+			]
+		}
 	}	
 )
